c69cfddfc8081c8f80ba341b29d22b50233b3b9c
Jeremy <jeremyshar@gmail.com>
2018-04-29 01:28:32 -0700

    Adds comments for some functions.

--- agents.py
+++ agents.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import numpy as np
 
+# An agent that makes completely random moves.
 class RandomAgent(object):
 	def __init__(self, name, game):
 		self.name = name
@@ -10,6 +11,7 @@ class RandomAgent(object):
 	def select_move(self):
 		return np.random.choice(self.game.legal_moves())
 
+# An agent that picks the first legal move.
 class DumbAgent(object):
 	def __init__(self, name, game):
 		self.name = name
@@ -18,6 +20,7 @@ class DumbAgent(object):
 	def select_move(self):
 		return self.game.legal_moves()[0]
 
+# An agent that plays optimally using minimax.
 class MinimaxAgent(object):
 	def __init__(self, name, game):
 		self.name = name
@@ -28,15 +31,19 @@ class MinimaxAgent(object):
 		return move
 
 	def minimax(self, game):
+		# If the game is over, return the value of the result.
 		if self.game.is_game_over():
 			return self.game.value_mapping[game.winner], None
+		# Initialize the best value to effectively +/-infinity, and the best move to invalid.
 		best_value = -10.0 if game.first_player_to_move else 10.0
 		best_move = -1
 		for move in self.game.legal_moves():
+			# Get the value of the subtree.
 			self.game.make_move(move)
 			value, _ = self.minimax(self.game)
 			self.game.undo_move()
 			change_detector = best_value
+			# Keep track of the best move.
 			best_value = max(best_value, value) if game.first_player_to_move else min(best_value, value)
 			if change_detector != best_value:
 				best_move = move
--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class TicTacToe(object):
 	def __init__(self, board_size, num_in_a_row):
+		# Mapping between potential game winners and outcome scores. _ is a draw.
 		self.value_mapping = {'_':0, 'X':1.0, 'O':-1.0}
 		self.empty = '_'
 		self.winner = self.empty
@@ -12,7 +13,9 @@ class TicTacToe(object):
 		self.first_player_to_move = True
 		self.board_size = board_size
 		self.num_in_a_row = num_in_a_row
+		# Initialize the board.
 		self.board = [[[self.empty] for i in range(board_size)] for j in range(board_size)]
+		# Columns and diagonals are just for semi-efficient indexing of the game.
 		self.columns = [[self.board[j][i] for j in range(board_size)] for i in range(board_size)]
 		self.diagonals = [[] for i in range((board_size*4)-2)]
 		for i in range(board_size):
@@ -21,7 +24,9 @@ class TicTacToe(object):
 				self.diagonals[(board_size-i-1)+j+(2*board_size)-1].append(self.board[i][j])
 		self.moves = []
 
+	# Checks if the game is over, and if it is, sets the game's winner.
 	def is_game_over(self):
+		# Iterate through the rows, columns and diagonals and check for 3 in a row.
 		for line in itertools.chain(self.board, self.columns, self.diagonals):
 			last_seen = self.empty
 			consecutive = 1
@@ -34,34 +39,43 @@ class TicTacToe(object):
 				else:
 					consecutive = 1
 				last_seen = elt[0]
+		# If there's no winner, check if it's a draw.
 		return len(self.legal_moves()) == 0
 
-
+	# Prints the current state of the game.
 	def print_board(self):
 		for row in self.board:
 			print '|' + '|'.join([elt[0] for elt in row]) + '|'
 
+	# Returns a list of the legal moves (flattened indices of empty squares).
 	def legal_moves(self):
 		return [i for i in range(self.board_size**2) 
 			if self.board[i//self.board_size][i%self.board_size][0] == self.empty]
 
+	# Makes a move in the game. The move should be the flattend index of an empty space.
 	def make_move(self, move):
 		self.board[move//self.board_size][move%self.board_size][0] = 'X' if self.first_player_to_move else 'O'
 		self.moves.append(move)
 		self.first_player_to_move = not self.first_player_to_move
 
+	# Undoes the last executed move.
 	def undo_move(self):
 		move = self.moves.pop()
 		self.winner = self.board[move//self.board_size][move%self.board_size][0] = self.empty
 		self.first_player_to_move = not self.first_player_to_move
 
+	# Resets the game to the initial state.
 	def reset(self):
 		self.winner = self.empty
 		self.first_player_to_move = True
+		self.moves = []
 		for i in range(self.board_size):
 			for j in range(self.board_size):
 				self.board[i][j][0] = self.empty
 
+	# Convenience function to transform the board state into a stack of layers.
+	# The first layer is the locations of X, the second O, and the third is entirely
+	# 1.0 if it's O-to-move and 0.0 if it's X-to-move.
 	def transform_state(self):
 		stack = np.zeros([self.board_size, self.board_size, 3])
 		if not self.first_player_to_move:
@@ -74,6 +88,7 @@ class TicTacToe(object):
 					stack[i][j][1] = 1.0
 		return stack
 
+# Infinite loop of play against an agent.
 def interactive_play(g, agent):
 	game = 0
 	while True:
@@ -101,6 +116,7 @@ def interactive_play(g, agent):
 			else:
 				print agent.name, 'wins!'
 
+# Runs a match between two agents.
 def play_match(g, agent_a, agent_b, num_games, print_games=False, print_results=True):
 	results = {agent_a.name : 0, 
 			   agent_b.name : 0, 
@@ -142,3 +158,4 @@ def play_match(g, agent_a, agent_b, num_games, print_games=False, print_results=
 		print overall_results
 		print results_breakdown
 	return overall_results, results_breakdown
+	
